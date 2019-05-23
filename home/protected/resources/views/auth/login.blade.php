7b26b7b62e7195f3aab591d269c7b83f8cfda833
minmon98 <phivanminh10@hotmail.com>
2019-05-23 10:49:23 +0700

    minhmon

--- home/protected/resources/views/auth/login.blade.php
+++ home/protected/resources/views/auth/login.blade.php
@@ -13,11 +13,33 @@
 		<img src="{{URL::asset('images/webv1.jpg')}}" alt="" class="img-fluid backgroundimage">
 		<div class="layer"></div>
 		<div class="logo">
-			<img src="{{URL::asset('images/logo-outline.png')}}" alt="" class="logoimage">
 			<p>u-Faculties</p>
 		</div>
-		<div class="loginbox">
-			
+		<div class="container">
+			<div class="row">
+				<div class="col-3"></div>
+				<div class="col-6">
+					<div class="loginbox">
+						<img src="{{URL::asset('images/logo-outline.png')}}" alt="" class="img-fluid logoimage">
+						<p class="title">Đăng nhập u-Faculties</p>
+						<form action="" class="loginform">
+							<fieldset>
+								<input type="email" id="acount" placeholder="Nhập email">
+							</fieldset>
+							<fieldset>
+								<div class="line" style="width: 100%; height: 2px; background: gray; margin: 0.5px 0px;"></div>
+							</fieldset>
+							<fieldset>
+								<input type="password" id="password" placeholder="Nhập mật khẩu">
+							</fieldset>
+							<div class="submit">
+								
+							</div>
+						</form>
+					</div>		
+				</div>
+				<div class="col-3"></div>
+			</div>
 		</div>
 	</div>
 	
