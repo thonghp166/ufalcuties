adb5e3fa73d0a219f96721732a97a0f4451250f9
thonghp166 <thonghp11@gmail.com>
2019-05-25 08:47:22 +0700

    update home link

--- home/protected/resources/views/staff/edit_info.blade.php
+++ home/protected/resources/views/staff/edit_info.blade.php
@@ -11,7 +11,7 @@
   
   
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark mt-3" id="bar">
-    <a class="navbar-brand" href="#" id="logo">
+    <a class="navbar-brand" href="{{route('home')}}" id="logo">
         <img src="{{URL::asset('images/logo-outline.png')}}" alt="Logo u-Faculties">
         u-Faculties
     </a>
