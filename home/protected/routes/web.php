a70b1e78c8c71dce80e063f8170ba7c94d9deede
linhng123 <linhnguyen.uet1210@gmail.com>
2019-05-29 14:41:03 +0700

    comment route

--- home/protected/routes/web.php
+++ home/protected/routes/web.php
@@ -1,75 +1,88 @@
 <?php
 use Illuminate\Support\Facades\Input;
-
-Route::get('/',[
-	'as' => 'home',
-	'uses' => 'HomeController@index'
-]);
+// Control to home function
 Route::get('/home','HomeController@index');
-
+// Search in home
 Route::get('/search',[
 	'as' => 'search',
 	'uses' => 'HomeController@search'
 ]);
+// Show staff's info
 Route::get('/staff/info/{account}',[
 	'as' => 'staff.info',
 	'uses' => 'StaffController@show'
 ]);
-
+// Paths for staff function
 Route::group(['middleware' => 'auth', 'prefix' => 'staff'], function() {
+	// Edit staff's info
 	Route::get('/edit/info','StaffController@edit')->name('staff.edit');
+	// Update staff's info which edited
 	Route::post('/edit/info','StaffController@updateInfo')->name('staff.update.info');
-
+	// Edit staff's field 
 	Route::get('/edit/field','StaffController@edit_field')->name('staff.edit.field');
+	// Update staff's field which edited
 	Route::post('/edit/field','StaffController@updateField')->name('staff.update.field');
-	
+	// Edit staff's topic 
 	Route::get('/edit/topic','StaffController@edit_topic')->name('staff.edit.topic');
-
+	// Add staff's topic 
 	Route::get('/add/topic','TopicController@add')->name('staff.add.topic');
+	// Delete staff's topic
 	Route::get('/delete/topic','TopicController@destroy')->name('staff.delete.topic');
+	// Update staff's topic
 	Route::get('/update/topic','TopicController@update')->name('staff.update.topic');
 
 });
-
+// Control to field function
 Route::get('/field','FieldController@index');
 Auth::routes();
+// Show form change user's password 
 Route::get('password/change',[
 	'middleware' => 'auth',
 	'as' => 'password.change',
 	'uses' => 'Auth\ChangePasswordController@showChangePasswordForm'
 ]);
+// Change user's password
 Route::post('password/change', [
 	'middleware' => 'auth',
 	'as' => 'password.change',
 	'uses' => 'Auth\ChangePasswordController@change'
 ]);
-
+// Paths for admin function
 Route::group(['middleware' => 'CheckAdminLogin', 'prefix' => 'admin'], function() {
+	// Control admin's home
 	Route::get('/', 'AdminController@index')->name('admin.home');
-
+	// Add admin department
 	Route::post('/','DepartmentController@store')->name('admin.add.department');
+	// Delete admin department
 	Route::get('/delete','DepartmentController@delete')->name('admin.delete.department');
+	// Delete admin department
 	Route::delete('/delete','DepartmentController@delete')->name('admin.delete.department');
+	// Update admin department
 	Route::post('/','DepartmentController@update')->name('admin.update.department');
-
+	// Add admin's field
 	Route::post('/','FieldController@addField')->name('admin.add.field');
+	// Delete admin's field
 	Route::post('/','FieldController@deleteField')->name('admin.delete.field');
+	// Update admin's field
 	Route::post('/','FieldController@updateField')->name('admin.update.field');
 
 	// Route::post('/','UserController@add')->name('admin.add.user');
+	// Admin delete user
 	Route::post('/','UserController@deleteUser')->name('admin.delete.user');
+	// Admin update user
 	Route::post('/','UserController@updateUser')->name('admin.update.user');
-
+	// Admin add user from excel table
 	Route::post('/import','UserController@importExcel')->name('admin.add.user.excel');
+	// Admin add user from data excel
 	Route::post('/adduser','UserController@addUser')->name('admin.add.user');
 });
 
-
+// View in home
 Route::get('/test',[
 	'as' => 'test',
 	'uses' => 'HomeController@test'
 ]);
-
+// Import file Excel into database
 Route::post('/test',[
 	'as' => 'import',
 	'uses' => 'UserController@importExcel'
