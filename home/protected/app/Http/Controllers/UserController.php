3188f1d58e35b74f27cb0d9d28cb974598d41ae2
minmon98 <phivanminh10@hotmail.com>
2019-06-01 11:17:23 +0700

    final admin

--- home/protected/app/Http/Controllers/UserController.php
+++ home/protected/app/Http/Controllers/UserController.php
@@ -138,13 +138,13 @@ class UserController extends Controller
                 'vnu_email' => $email,
                 'user_id' => $user->id,
                 'account' => $username,
+                'code' => 'NEW'.$user->id
             ]);
             $account_info = [
                 'username' => $username,
                 'email' => $email,
                 'name' => $name,
-                'id' => $user->id,
-                'code' => 'NEW'.$user->id
+                'id' => $user->id
             ];
             return $account_info;
         } else {
